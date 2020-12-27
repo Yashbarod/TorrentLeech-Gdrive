596dc9ca4dcc026fe5de8563bf918c278889c234
Yashbarod <67955619+Yashbarod@users.noreply.github.com>
2020-12-27 22:34:32 +0530

    Update and rename g_config.py to config.py

---
+++ tobrot/config.py
@@ -0,0 +1,16 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+    TG_BOT_TOKEN= "1001428307:AAH8ZZrlJR2lXc1gnsKek4TBaf3JMwNtu-w"
+    APP_ID = 1601946
+    API_HASH = "09467a765c24de169a965b671f16a387"
+    OWNER_ID = 618579098
+    AUTH_CHANNEL = [-1001371735106]
+    DESTINATION_FOLDER = "IYDL Bot" #Name of your folder read readme(not id of the folder)
+    RCLONE_CONFIG = """type = drive
+scope = drive
+token = {"access_token":"ya29.a0AfH6SMB0xu3rCe0CVQwVkch1w2pdpFSAeI7ikt3oGsq-ywVUswnq9AIBmeYpGgD8xmXwpsvztNc-vyiq1bIHbMq3VzLdl8aLRA0D2iebL9ppNCFaKnVqSyQ1Y6buun4KGdyWUqj_eiunpv2ql26da8zy87bMfkXHc3U","token_type":"Bearer","refresh_token":"1//0gxnAT17c3AE9CgYIARAAGBASNwF-L9IrKS5D2XL7noEra_2B-sOaP2itbUwO-pyZPvCajRTPPeUabFCOX2newmwi-uuTIV2yEJ0","expiry":"2020-07-07T17:37:02.4771735+05:30"}
+root_folder_id = 10MK__ekLdXD4M_IlilYwnLm-86ZHnPiw"""
+    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
+    #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
