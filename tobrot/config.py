69967eaae8155ff6888dca843b1368792d0baf3d
Yashbarod <67955619+Yashbarod@users.noreply.github.com>
2020-12-27 23:37:05 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -10,7 +10,6 @@ class Config(Config):
     DESTINATION_FOLDER = "IYDL Bot" #Name of your folder read readme(not id of the folder)
     RCLONE_CONFIG = """type = drive
 scope = drive
-root_folder_id = 10MK__ekLdXD4M_IlilYwnLm-86ZHnPiw
 token = {"access_token":"ya29.a0AfH6SMDNnEVvKI4k-XrJhVjdyzaUXCRszyg0A52nQijYcM_CdRNP9SXni9ahML7WVxHznRUrwr1f9JUMd1-ufFj2oRYSj6dPt8YoEqfKmp2ZKb19t80f1xfhJbzxicMHF5NAaPT5AtX1nZsif0ftoUa64V-u13UGc9iXLMckq9U","token_type":"Bearer","refresh_token":"1//0grwIssQPRFQFCgYIARAAGBASNwF-L9IrI-9jWPkyjEY6oDlHypfUL7pQgBIN233D5Av4WisXbehviCzelJrGbJVragQ_QGTxgi0","expiry":"2020-12-27T18:49:32.37751619Z"}
 team_drive = 0AKEIlmAviSz1Uk9PVA"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
